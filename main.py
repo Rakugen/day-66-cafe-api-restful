11053b2688dab8fcdac0a2012ee6fae2d720edc4
Rakugen <simonlee0306@gmail.com>
2022-12-28 18:09:50 -0500

    Added random cafe method, to_dict method to Cafe model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
@@ -23,11 +24,39 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # dictionary = {}
+        # for column in self.__table__.columns:
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
+
+        return {column.name: getattr(self,column.name) for column in self.__table__.columns}
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+
+    # Use jsonify() to turn SQLAlchemy object into a JSON via serialization:
+    # new_json = jsonify(cafe={
+    #     'can_take_calls': random_cafe.can_take_calls,
+    #     'coffee_price': random_cafe.coffee_price,
+    #     'has_sockets': random_cafe.has_sockets,
+    #     'has_toilet': random_cafe.has_toilet,
+    #     'has_wifi': random_cafe.has_wifi,
+    #     # 'id': random_cafe.id,
+    #     'img_url': random_cafe.img_url,
+    #     'location': random_cafe.location,
+    #     'map_url': random_cafe.map_url,
+    #     'name': random_cafe.name,
+    #     'seats': random_cafe.seats
+    #     }
+    # )
+    return jsonify(cafe=random_cafe.to_dict())
 
 ## HTTP GET - Read Record
 
